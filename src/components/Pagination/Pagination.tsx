ab6362f22185f929b9166194630673070a494927
kimsunin <suninkim10@gmail.com>
2024-08-20 21:47:48 +0900

    feat:add Pagination component and function

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,25 @@
+"use client"
+import styles from "./Pagination.module.css"
+import {DotIc, LeftIc, RightIc} from "svg/index";
+import {useRouter} from "next/navigation";
+
+type PropsType = {
+  p?:number,
+  q?:string
+}
+
+function Pagination({p, q}: PropsType) {
+  const router = useRouter();
+
+  return <div className={styles.pagination}>
+    <button onClick={() => {
+      if (p && p > 1) {
+        router.push(`/book/${p - 1}/${q ? `${q}` : ""}`)
+      }
+    }}><LeftIc/></button>
+    <DotIc/>
+    <button onClick={() => router.push(`/book/${Number(p) + 1}/${q ? `${q}` : ""}`)}><RightIc/></button>
+  </div>;
+}
+
+export default Pagination;
